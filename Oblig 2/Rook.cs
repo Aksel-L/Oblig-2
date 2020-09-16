d33a62d33ce23541161d7a52f9bf5b63fcced4d6
Aksel-L <62015123+Aksel-L@users.noreply.github.com>
2020-09-16 17:38:05 +0200

    a

---
+++ Oblig 2/Rook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SimpleChess;
+
+namespace Oblig_2
+{
+    public class Rook : Piece
+    {
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+
+        public Rook(string symbol) : base(symbol)
+        {
+        }
+    }
+}
